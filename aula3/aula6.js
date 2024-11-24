4ea94c7468287a6f9b93aa9ed6434bd8e5adc0d9
Ariaman-gomes <148455368+Ariaman-gomes@users.noreply.github.com>
2024-11-24 10:31:11 -0300

    atualizado

---
+++ aula3/aula6.js
@@ -0,0 +1,15 @@
+let n1=1
+let n2=2
+
+console.log(!(n1!=n2))
+
+//let nome=prompt("digite seu nime")
+
+//let n1=1
+//let n2="1"
+
+//let v1={nome:"Ary"}
+//let v2=v1//{nome:"Ary"}
+//console.log(n1==n2)
+//console.log(n1==n2)
+//console.log(nome)
