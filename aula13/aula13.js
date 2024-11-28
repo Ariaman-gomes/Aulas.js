1e1b99279bffca910ad8c962c99a913c84889b96
Ariaman-gomes <148455368+Ariaman-gomes@users.noreply.github.com>
2024-11-28 17:40:19 -0300

    atualizado

---
+++ aula13/aula13.js
@@ -0,0 +1,15 @@
+let num=100
+
+if(num > 10){
+    console.log("numeral maior que 10")
+//console.log("comando 2")
+
+} else if(num> 5) {
+    console.log("numeral entre 6 e 10")
+}
+else{ 
+    console.log("Numeral, menor ou igual a 5")
+}
+
+
+console.log("fim do programa")
