c856c108d56f83dcab61b2f68a3350d49b886b27
Ariaman-gomes <148455368+Ariaman-gomes@users.noreply.github.com>
2024-11-30 10:18:54 -0300

    aatualizado

---
+++ aula19/aula1/aula19.js
@@ -0,0 +1,31 @@
+let n=0
+let max=1000
+let pares=0
+
+for(let i=n;i<max;i++){
+    //console.log("GA-GEBESIS -"+ i)
+
+    if(i%2!==0){
+        //pares++
+        continue
+    }
+    pares++
+}
+console.log("Quantidade de pares: " + pares)
+
+console.log("Fim do programa!")
+
+
+
+
+/*let n=0         ESTRUTURA BREAK
+/* 
+
+while(n<max){
+    console.log("GA genesis - " + n)
+    if(n>10){
+        break
+    }
+    n++
+}
+console.log('Fim do programa!')*/
