ab82174e16eace1aba917b745ca1fd761f83e32d
Ariaman-gomes <148455368+Ariaman-gomes@users.noreply.github.com>
2024-12-01 11:12:00 -0300

    atualizado

---
+++ aula20/aula1/aula20.js
@@ -0,0 +1,27 @@
+//function soma2_10()
+function mudarTexto(){
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML="Ary Silva"
+    d2.innerHTML="Ary Silva"
+    d3.innerHTML="Ary Silva"
+
+    /*let n1=2
+    let n2=10
+    let soma=n1+n2
+    console.log(soma)
+    console.log("ary gomes")*/
+}
+//mudarTexto()
+/*for(let i=0;i<10;i++){
+    soma2_10()
+}
+console.log("FIM!")
+nome()
+nome()
+nome()
+nome()
+nome()
+nome()
+nome()*/
