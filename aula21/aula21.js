b1d5ed135ac5143b816e5aeaab4f160093e4369b
Ariaman-gomes <148455368+Ariaman-gomes@users.noreply.github.com>
2024-12-02 14:21:39 -0300

    atualizado

---
+++ aula21/aula21.js
@@ -0,0 +1,17 @@
+function canal(){
+    //return "ARY gomes"
+    let n1=10
+    let n2=2
+    let res=n1*n2
+    if(res%2==0)
+        return"par"
+    else
+        return "impar"
+    //return res
+
+}
+let res=canal()
+console.log(res)
+/*console.log(canal())
+console.log(canal())
+console.log(canal())*/
